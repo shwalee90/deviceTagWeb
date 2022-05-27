02535b5dc88ce2daf567c39ef3d8591725597cb3
DESKTOP-LBV8K65\namoo <qnfrgmfr16@gmail.com>
2022-05-27 13:45:09 +0900

    멀티모듈 시도 .. (yarn eject 위한 푸쉬 )

---
+++ module-middle/src/main/java/com/auxil/pump/MiddleApplication.java
@@ -0,0 +1,16 @@
+package com.auxil.pump;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiddleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiddleApplication.class, args);
+    }
+
+
+
+
+}
