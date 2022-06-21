cad33aa61a6326e695b8871f2e6f929bd6c29069
DESKTOP-LBV8K65\namoo <qnfrgmfr16@gmail.com>
2022-06-21 13:03:47 +0900

    유레카 게이트웨이 제거

--- module-pump/src/test/java/com/auxil/pump/template/RepositoryTestTemplate.java
+++ module-pump/src/test/java/com/auxil/pump/template/RepositoryTestTemplate.java
@@ -1,6 +1,7 @@
 package com.auxil.pump.template;
 
 import org.junit.jupiter.api.Disabled;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
